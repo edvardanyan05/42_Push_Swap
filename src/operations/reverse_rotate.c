8a1e171f512f1122984fa5ed1d57376bc1e9b1af
MelineGogjyan <gogjyanmeline@gmail.com>
2026-03-26 19:00:25 +0400

    Revers Rotate Operations

---
+++ src/operations/reverse_rotate.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 18:36:10 by mgogjyan          #+#    #+#             */
+/*   Updated: 2026/03/26 18:59:15 by mgogjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rev_rotate(t_list **stack)
+{
+	t_list	*it;
+	t_list	*tmp;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	it = *stack;
+	while (it->next->next)
+		it = it->next;
+	tmp = it->next;
+	it->next = NULL;
+	tmp->next = *stack;
+	*stack = tmp;
+}
+
+void	rra(t_list **a)
+{
+	if (!a || !*a || !(*a)->next)
+		return ;
+	rev_rotate(a);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_list **b)
+{
+	if (!b || !*b || !(*b)->next)
+		return ;
+	rev_rotate(b);
+	write(1, "rrb\n", 4);
+}
+
+void	rrr(t_list **a, t_list **b)
+{
+	if ((!a || !*a || !(*a)->next) && (!b || !*b || !(*b)->next))
+		return ;
+	rev_rotate(a);
+	rev_rotate(b);
+	write(1, "rrr\n", 4);
+}
