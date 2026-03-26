e65d05ad60273c7eac6667d213a6cbd2ccff2032
MelineGogjyan <gogjyanmeline@gmail.com>
2026-03-26 18:33:15 +0400

    Swap Operations

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -6,7 +6,7 @@
 /*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 19:11:43 by edmvarda          #+#    #+#             */
-/*   Updated: 2026/03/26 17:58:32 by mgogjyan         ###   ########.fr       */
+/*   Updated: 2026/03/26 18:31:18 by mgogjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,25 @@ static void	swap_top(t_list **stack)
 
 void	sa(t_list **a)
 {
-	if (a && *a && (*a)->next)
-	{
-		swap_top(a);
-		write(1, "sa\n", 3);
-	}
+	if (!a || !*a || !(*a)->next)
+		return ;
+	swap_top(a);
+	write(1, "sa\n", 3);
 }
 
 void	sb(t_list **b)
 {
-	if (b && *b && (*b)->next)
-	{
-		swap_top(b);
-		write(1, "sb\n", 3);
-	}
+	if (!b || !*b || !(*b)->next)
+		return ;
+	swap_top(b);
+	write(1, "sb\n", 3);
 }
 
 void	ss(t_list **a, t_list **b)
 {
-	if ((a && *a && (*a)->next) || (b && (*b) && (*b)->next))
-	{
-		swap_top(a);
-		swap_top(b);
-		write(1, "ss\n", 3);
-	}
+	if ((!a || !*a || !(*a)->next) && (!b || !*b || !(*b)->next))
+		return ;
+	swap_top(a);
+	swap_top(b);
+	write(1, "ss\n", 3);
 }
