3d85d07ebe4a9ff3d5abdc039dd043a85d47c312
MelineGogjyan <gogjyanmeline@gmail.com>
2026-03-28 18:54:18 +0400

    Header File

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 20:48:08 by edmvarda          #+#    #+#             */
-/*   Updated: 2026/03/28 15:47:11 by mgogjyan         ###   ########.fr       */
+/*   Updated: 2026/03/28 18:33:33 by mgogjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,6 @@
 
 # include "libft.h"
 # include <limits.h>
-# include <unistd.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
 
 void				sa(t_list **a);
 void				sb(t_list **b);
