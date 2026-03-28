6ab8e19b826aed5243edaf6925925401409c46d7
Edmond Vardanyan <edmvarda@c2r11s6.42yerevan.am>
2026-03-28 23:16:18 +0400

    Parsing Logic

--- src/parsing/ parse_args.c
+++ src/parsing/ parse_args.c
@@ -3,28 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*    parse_args.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edmvarda <edmvarda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 19:02:21 by mgogjyan          #+#    #+#             */
-/*   Updated: 2026/03/28 18:48:22 by mgogjyan         ###   ########.fr       */
+/*   Updated: 2026/03/28 20:16:40 by edmvarda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	valid_checker(int ac, char **av)
-{
-	int	arg_i;
-
-	arg_i = 1;
-	while (arg_i < ac)
-	{
-		if (!is_valid(av[arg_i]))
-			return (0);
-		arg_i++;
-	}
-	return (1);
-}
 
 t_list	*fill_stack(int ac, char **av)
 {
@@ -39,11 +26,17 @@ t_list	*fill_stack(int ac, char **av)
 	while (arg_i < ac)
 	{
 		matrix = ft_split(av[arg_i], ' ');
+		if (!matrix)
+			return (NULL);
 		matrix_i = 0;
 		while (matrix[matrix_i])
 		{
 			new_node = (t_list *)malloc(sizeof(t_list));
+			if (!new_node)
+				return (NULL);
 			new_node->content = malloc(sizeof(int));
+			if (!new_node || !new_node->content)
+				return (NULL);
 			*(int *)new_node->content = ft_atoi(matrix[matrix_i]);
 			ft_lstadd_back(&stack, new_node);
 			matrix_i++;
