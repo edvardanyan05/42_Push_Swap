0cf36b1ecb6dfa739c4f79a2d9be0971e33294d0
Edmond Vardanyan <edmondvar2005@gmail.com>
2026-04-01 23:14:34 +0400

    Sorting Preparation

--- src/utils/sorting_utils.c
+++ src/utils/sorting_utils.c
@@ -11,3 +11,59 @@ int is_sorted(t_list *stack)
     }
     return (1);
 }
+
+static int get_rank(t_list *stack, t_list *node)
+{
+    int count;
+
+    count = 0;
+    while(stack)
+    {
+        if(*(int *)node->content > *(int *)stack->content)
+            count++;
+        stack = stack->next;
+    }
+    return (count);
+}
+
+int* assign_indexes(t_list *stack, int size)
+{
+    t_list  *it;
+    int     *arr;
+    int     pos;
+
+    arr = malloc(size * sizeof(int));
+    if (!arr)
+        return NULL;
+    it = stack;
+    pos = 0;
+    while(it)
+    {
+        arr[pos++] = get_rank(stack, it);
+        it = it->next;
+    }
+    return(arr);
+}
+
+double  compute_disorder(t_list *stack)
+{
+    t_list *it;
+    int mistakes;
+    int total_pairs;
+
+    mistakes = 0;
+    total_pairs = 0;
+    while (stack)
+    {
+        it = stack->next;
+        while(it)
+        {
+            total_pairs++;
+            if (*(int *)stack->content > *(int *)it->content)
+                mistakes++;
+            it = it->next; 
+        }
+        stack = stack->next;
+    }
+    return ((double)mistakes / total_pairs);
+}
