fd3870fbe240be09161d8b5f0a1251cece6d56c8
MelineGogjyan <gogjyanmeline@gmail.com>
2026-04-06 14:21:41 +0400

    Sorting & Sorting Utils

---
+++ src/sorting/sort_small.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_small.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 19:02:21 by mgogjyan          #+#    #+#             */
+/*   Updated: 2026/04/06 14:10:28 by mgogjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_two(t_list **a)
+{
+	if (*(int *)(*a)->content > *(int *)(*a)->next->content)
+		sa(a);
+}
+
+void	sort_three(t_list **a)
+{
+	int	*arr;
+
+	arr = assign_indexes(*a, 3);
+	if (!arr)
+		return ;
+	if (arr[0] == 0 && arr[1] == 1)
+	{
+		free(arr);
+		return ;
+	}
+	else if (arr[0] == 0 && arr[1] == 2)
+	{
+		rra(a);
+		sa(a);
+	}
+	else if (arr[0] == 1 && arr[1] == 0)
+		sa(a);
+	else if (arr[0] == 1 && arr[1] == 2)
+		rra(a);
+	else if (arr[0] == 2 && arr[1] == 0)
+		ra(a);
+	else if (arr[0] == 2 && arr[1] == 1)
+	{
+		sa(a);
+		rra(a);
+	}
+	free(arr);
+}
+
+static int	find_rank_pos(int *arr, int size, int rank)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (arr[i] == rank)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static void	push_min_to_b(t_list **a, t_list **b)
+{
+	int	*arr;
+	int	size;
+	int	pos;
+	int	steps;
+
+	size = ft_lstsize(*a);
+	arr = assign_indexes(*a, size);
+	if (!arr)
+		return ;
+	pos = find_rank_pos(arr, size, 0);
+	free(arr);
+	if (pos <= size / 2)
+	{
+		steps = pos;
+		while (steps-- > 0)
+			ra(a);
+	}
+	else
+	{
+		steps = size - pos;
+		while (steps-- > 0)
+			rra(a);
+	}
+	pb(a, b);
+}
+
+void	sort_five(t_list **a, t_list **b)
+{
+	if (ft_lstsize(*a) != 5)
+		return ;
+	push_min_to_b(a, b);
+	push_min_to_b(a, b);
+	sort_three(a);
+	if (*(int *)(*b)->content > *(int *)(*b)->next->content)
+		sb(b);
+	pa(b, a);
+	pa(b, a);
+}
--- src/utils/sorting_utils.c
+++ src/utils/sorting_utils.c
@@ -1,69 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgogjyan <mgogjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 20:15:34 by edmvarda          #+#    #+#             */
+/*   Updated: 2026/04/06 14:10:26 by mgogjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
 
-int is_sorted(t_list *stack)
+int	is_sorted(t_list *stack)
 {
-    while (stack && stack->next)
-    {
-        if (*(int *)stack->content > *(int *)stack->next->content)
-            return (0);
-        stack = stack->next;
-    }
-    return (1);
+	while (stack && stack->next)
+	{
+		if (*(int *)stack->content > *(int *)stack->next->content)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
 }
 
-static int get_rank(t_list *stack, t_list *node)
+static int	get_rank(t_list *stack, t_list *node)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    while(stack)
-    {
-        if(*(int *)node->content > *(int *)stack->content)
-            count++;
-        stack = stack->next;
-    }
-    return (count);
+	count = 0;
+	while (stack)
+	{
+		if (*(int *)node->content > *(int *)stack->content)
+			count++;
+		stack = stack->next;
+	}
+	return (count);
 }
 
-int* assign_indexes(t_list *stack, int size)
+int	*assign_indexes(t_list *stack, int size)
 {
-    t_list  *it;
-    int     *arr;
-    int     pos;
+	t_list	*it;
+	int		*arr;
+	int		pos;
 
-    arr = malloc(size * sizeof(int));
-    if (!arr)
-        return NULL;
-    it = stack;
-    pos = 0;
-    while(it)
-    {
-        arr[pos++] = get_rank(stack, it);
-        it = it->next;
-    }
-    return(arr);
+	arr = malloc(size * sizeof(int));
+	if (!arr)
+		return (NULL);
+	it = stack;
+	pos = 0;
+	while (it)
+	{
+		arr[pos++] = get_rank(stack, it);
+		it = it->next;
+	}
+	return (arr);
 }
 
-double  compute_disorder(t_list *stack)
+double	compute_disorder(t_list *stack)
 {
-    t_list *it;
-    int mistakes;
-    int total_pairs;
+	t_list	*it;
+	int		mistakes;
+	int		total_pairs;
 
-    mistakes = 0;
-    total_pairs = 0;
-    while (stack)
-    {
-        it = stack->next;
-        while(it)
-        {
-            total_pairs++;
-            if (*(int *)stack->content > *(int *)it->content)
-                mistakes++;
-            it = it->next; 
-        }
-        stack = stack->next;
-    }
-    return ((double)mistakes / total_pairs);
+	mistakes = 0;
+	total_pairs = 0;
+	while (stack)
+	{
+		it = stack->next;
+		while (it)
+		{
+			total_pairs++;
+			if (*(int *)stack->content > *(int *)it->content)
+				mistakes++;
+			it = it->next;
+		}
+		stack = stack->next;
+	}
+	return ((double)mistakes / total_pairs);
 }
