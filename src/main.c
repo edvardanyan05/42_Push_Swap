6fd385617b835473d5df108a5f746f90f6111b03
Edmond Vardanyan <edmondvar2005@gmail.com>
2026-04-12 02:58:44 +0400

    Main Function

--- src/main.c
+++ src/main.c
@@ -10,3 +10,58 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+    int arg_i;
+    int bench;
+    t_list *stack;
+    t_list *reserve;
+
+    t_strategy  commands;
+
+    if (argc < 2)
+        return (0);
+    arg_i = 1;
+    commands = ADAPTIVE;
+    if(argc > arg_i && argv[arg_i][0] == '-' && argv[arg_i][1] == '-')
+    {
+        if(ft_strncmp(argv[1], "--adaptive", 10) == 0)
+            commands = ADAPTIVE;
+        else if(ft_strncmp(argv[1], "--simple", 8) == 0)
+            commands = SIMPLE;
+        else if(ft_strncmp(argv[1], "--medium", 8) == 0)
+            commands = MEDIUM;
+        else if(ft_strncmp(argv[1], "--complex", 9) == 0)
+            commands = COMPLEX;
+        else
+        {
+            write(2, "Error\n", 6);
+            return (0);
+        }
+        arg_i++;
+    }
+    bench = 0;
+    if (argc > arg_i && ft_strncmp(argv[arg_i], "--bench", 7) == 0)
+    {
+        bench = 1;
+        arg_i++;
+    }
+    stack = pars_args(argc - arg_i + 1, argv + arg_i - 1);
+    if (!stack)
+        return (0);
+    if (is_sorted(stack))
+        return (delete_stack(stack), 0);
+    reserve = NULL;
+    if (commands == SIMPLE)
+        simple_sort(&stack, &reserve);
+    else if (commands == MEDIUM)
+        chunk_sort(&stack, &reserve);
+    else if (commands == COMPLEX)
+        radix_sort(&stack, &reserve);
+    else
+    {
+        
+    }
+}
